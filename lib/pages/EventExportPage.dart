b921cc7060959011d585b73e0cf2791b5a48e22b
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2024-07-02 10:48:27 +0200

    feat: Save QR code as image

--- lib/pages/EventExportPage.dart
+++ lib/pages/EventExportPage.dart
@@ -1,11 +1,17 @@
+import 'dart:io';
+
 import 'package:cptclient/json/credential.dart';
 import 'package:cptclient/json/event.dart';
 import 'package:cptclient/json/session.dart';
 import 'package:cptclient/material/AppBody.dart';
+import 'package:cptclient/static/html.dart' if (dart.library.html) 'dart:html' as html;
+import 'package:file_selector/file_selector.dart' as file_selector;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class EventExportPage extends StatefulWidget {
@@ -26,6 +32,8 @@ class EventExportPage extends StatefulWidget {
 
 class EventExportPageState extends State<EventExportPage> {
   String url = "";
+  final ScreenshotController _ctrlSceenshot = ScreenshotController();
+
   EventExportPageState();
 
   @override
@@ -45,6 +53,27 @@ class EventExportPageState extends State<EventExportPage> {
     setState(() => url = "$scheme://$host:$port/#/login_event?key=$key&pwd=$pwd");
   }
 
+  _handleQR() async {
+    String fileName = "CPT_QR_event_${widget.credits.login}";
+    Uint8List? image;
+
+    await _ctrlSceenshot.capture().then((capturedImage) => image = capturedImage);
+
+    if (kIsWeb) {
+      html.AnchorElement()
+        ..href = '${Uri.dataFromBytes(image as List<int>, mimeType: 'image/png')}'
+        ..download = fileName
+        ..style.display = 'none'
+        ..click();
+    } else {
+      String? outputPath = (await file_selector.getSaveLocation(suggestedName: fileName))?.path;
+      if (outputPath != null) {
+        final outputFile = File(outputPath);
+        await outputFile.writeAsBytes(image as List<int>);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,12 +82,20 @@ class EventExportPageState extends State<EventExportPage> {
       ),
       body: AppBody(
         children: [
-          Center(
-            child: QrImageView(
-              data: url,
-              version: QrVersions.auto,
-              size: 200.0,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Screenshot(
+                controller: _ctrlSceenshot,
+                child: QrImageView(
+                  data: url,
+                  version: QrVersions.auto,
+                  size: 256.0,
+                  gapless: false,
+                ),
+              ),
+              IconButton(onPressed: _handleQR, icon: Icon(Icons.screenshot)),
+            ],
           ),
           ListTile(
             dense: true,
@@ -66,7 +103,7 @@ class EventExportPageState extends State<EventExportPage> {
             trailing: IconButton(
               icon: Icon(Icons.copy_rounded),
               onPressed: () {
-                Clipboard.setData(ClipboardData(text: url)).then((_){
+                Clipboard.setData(ClipboardData(text: url)).then((_) {
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("URL copied to clipboard")));
                 });
               },
---
+++ lib/static/html.dart
@@ -0,0 +1,8 @@
+// Dummy object for a conditional html package
+
+class AnchorElement {
+  set href(String str) {}
+  set download(String str) {}
+  dynamic get style => throw UnimplementedError();
+  void click() {}
+}
