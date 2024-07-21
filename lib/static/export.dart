ee866804248d02e6183e7c6950a00de7ced98440
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2024-07-21 08:56:26 +0200

    feat: Unicode export

--- lib/static/export.dart
+++ lib/static/export.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:cptclient/static/html.dart' if (dart.library.html) 'dart:html' as html;
@@ -5,8 +6,6 @@ import 'package:file_selector/file_selector.dart' as file_selector;
 import 'package:flutter/foundation.dart';
 
 exportQR(String filename, Uint8List image) async {
-
-
   if (kIsWeb) {
     html.AnchorElement()
       ..href = '${Uri.dataFromBytes(image as List<int>, mimeType: 'image/png')}'
@@ -27,7 +26,7 @@ exportCSV(String fileName, List<String> table) async {
 
   if (kIsWeb) {
     html.AnchorElement()
-      ..href = '${Uri.dataFromString(content, mimeType: 'text/csv')}'
+      ..href = '${Uri.dataFromString(content, mimeType: 'text/csv', encoding: utf8)}'
       ..download = fileName
       ..style.display = 'none'
       ..click();
@@ -35,7 +34,7 @@ exportCSV(String fileName, List<String> table) async {
     String? outputPath = (await file_selector.getSaveLocation(suggestedName: fileName))?.path;
     if (outputPath != null) {
       final outputFile = File(outputPath);
-      await outputFile.writeAsString(content);
+      await outputFile.writeAsString(content, encoding: const Utf8Codec());
     }
   }
 }
