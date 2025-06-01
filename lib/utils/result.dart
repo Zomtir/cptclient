cc1d8940daca5064bf40358715eace28a5d61219
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2025-06-01 12:32:07 +0200

    feat: Result as sealed return type

---
+++ lib/utils/result.dart
@@ -0,0 +1,12 @@
+sealed class Result<T> {
+  const Result();
+}
+
+class Success<T> extends Result<T> {
+  final T value;
+  const Success(this.value);
+}
+
+class Failure<T> extends Result<T> {
+  const Failure();
+}
