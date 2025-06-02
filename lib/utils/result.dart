46efbdff7b6ee6cfad5bf1ab50518fd8d42af125
Moritz Oberhauser <moritz.oberhauser@gmail.com>
2025-06-02 12:13:59 +0200

    feat: Unwrapping for Result

--- lib/utils/result.dart
+++ lib/utils/result.dart
@@ -1,5 +1,12 @@
 sealed class Result<T> {
   const Result();
+
+  T unwrap() {
+    if (this is Success<T>) {
+      return (this as Success<T>).value;
+    }
+    throw Exception('Tried to unwrap a Failure');
+  }
 }
 
 class Success<T> extends Result<T> {
